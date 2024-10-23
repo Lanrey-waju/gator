6177c0f546dc3ebb635bc2e829a3336be15dffa1
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-10-23 06:53:46 +0100

    add register handler and refine login handler

--- command.go
+++ command.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/Lanrey-waju/gator.git/internal/config"
+	"github.com/Lanrey-waju/gator.git/internal/database"
+	"github.com/google/uuid"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -37,10 +43,43 @@ func loginHandler(s *state, cmd command) error {
 		return errors.New("gator expects at least one argument: the username")
 	}
 	username := cmd.arg[0]
-	err := s.cfg.SetUser(username)
+	user, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		return fmt.Errorf("error: %v", err)
+	}
+	err = s.cfg.SetUser(user.Name)
 	if err != nil {
 		return err
 	}
 	fmt.Printf("%s has been set", username)
 	return nil
+}
+
+func registerHandler(s *state, cmd command) error {
+	if len(cmd.arg) < 1 {
+		return errors.New("gator expects at least one argument: the username")
+	}
+	username := cmd.arg[0]
+	user, err := s.db.GetUser(context.Background(), username)
+	if err == sql.ErrNoRows {
+		user, err = s.db.CreateUser(context.Background(), database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Name:      username,
+		})
+		if err != nil {
+			return fmt.Errorf("error: %v", err)
+		}
+		if err = s.cfg.SetUser(username); err != nil {
+			return fmt.Errorf("error: %v", err)
+		}
+		fmt.Printf("%s was created. ID: %s, Created At: %v, Updated At: %v, name: %v", username, user.ID, user.CreatedAt, user.UpdatedAt, user.Name)
+		return nil
+	} else if err != nil {
+		return fmt.Errorf("Database error: %v", err)
+	} else {
+		return fmt.Errorf("user already exists")
+	}
+
 }
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/Lanrey-waju/gator.git/internal/config"
+	"github.com/Lanrey-waju/gator.git/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -21,10 +25,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error reading config: %v", err)
 	}
-	s := state{cfg: &cfg}
+	db, err := sql.Open("postgres", cfg.DBUrl)
+	if err != nil {
+		log.Fatal("Error connecting to database")
+	}
+
+	dbQueries := database.New(db)
+
+	s := state{db: dbQueries, cfg: &cfg}
 
 	cmds := commands{commandHandlers: make(map[string]func(*state, command) error)}
 	cmds.register("login", loginHandler)
+	cmds.register("register", registerHandler)
 
 	if err := cmds.run(&s, cmd); err != nil {
 		log.Fatalf("Error running command: %s", err)
